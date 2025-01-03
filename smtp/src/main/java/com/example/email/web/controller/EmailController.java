c95838e619231c7608ee88bab13f300b67c54daa
Dima Artamonov <artamonovdima355@gmail.com>
2025-01-03 17:08:23 +0200

    Update k8s manifests

--- smtp/src/main/java/com/example/email/web/controller/EmailController.java
+++ smtp/src/main/java/com/example/email/web/controller/EmailController.java
@@ -1,23 +1,18 @@
 package com.example.email.web.controller;
 
 import com.example.email.common.HttpResponse;
+import com.example.email.common.HttpResponseDirector;
 import com.example.email.common.HttpResponseEmailBuilder;
 import com.example.email.domain.MailBox;
 import com.example.email.service.EmailSenderTemplate;
 import com.example.email.service.EmailService;
-import com.example.email.service.impl.AwsS3StorageService;
 import com.example.email.web.dto.EmailCreationDto;
 import com.example.email.web.mapper.EmailMapper;
 import jakarta.mail.MessagingException;
-import jakarta.validation.Valid;
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 import java.security.Principal;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,12 +30,14 @@ public class EmailController {
 
     @PostMapping
     public ResponseEntity<HttpResponse> sendEmail(
-            @RequestPart(value ="file", required = false) MultipartFile file,
+            @RequestPart(value = "file", required = false) MultipartFile file,
             @RequestPart(value = "emailCreationDto") EmailCreationDto emailCreationDto,
             Principal principal) throws IOException, MessagingException {
-                emailSenderTemplate.sendEmailTemplate(emailMapper.toEmail(emailCreationDto), principal.getName(),
-                        file);
-        return ResponseEntity.ok(new HttpResponseEmailBuilder().build());
+        emailSenderTemplate.sendEmailTemplate(emailMapper.toEmail(emailCreationDto),
+                principal.getName(),
+                file);
+        HttpResponseDirector director = new HttpResponseDirector(new HttpResponseEmailBuilder());
+        return ResponseEntity.ok(director.constructResponse());
     }
 
 
